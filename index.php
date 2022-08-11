f6070475722f684891dd1287e8e6e856426a4319
arkadhost <83273378+arkadhost@users.noreply.github.com>
2022-08-11 06:27:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $token = $_GET['token'];
-$iframe = '<html><head></head><body style="padding: 0;margin: 0;overflow: hidden;"> <iframe src= "run.php?token='.$token.'" style="padding: 0; margin: 0; overflow: hidden; border: 0px #ffffff none; height: 100vh; width: 100vw;"/> <script disable-devtool-auto src="https://fastly.jsdelivr.net/npm/disable-devtool/disable-devtool.min.js"></script></body></html>';
+$iframe = '<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></head><body style="padding: 0;margin: 0;overflow: hidden;"> <iframe src= "run.php?token='.$token.'" style="padding: 0; margin: 0; overflow: hidden; border: 0px #ffffff none; height: 100vh; width: 100vw;"/> <script disable-devtool-auto src="https://fastly.jsdelivr.net/npm/disable-devtool/disable-devtool.min.js"></script></body></html>';
 echo $iframe;
 ?>
